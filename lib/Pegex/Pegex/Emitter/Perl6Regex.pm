a05738c0b28076dcfd6a21a90e702e71e9b88b44
Ingy dot Net <ingy@ingy.net>
2011-09-30 10:51:59 +0200

    First commit

---
+++ lib/Pegex/Pegex/Emitter/Perl6Regex.pm
@@ -0,0 +1,22 @@
+##
+# name:      Pegex::Pegex::Emitter::Perl6Regex
+# abstract:  
+# author:    Ingy döt Net <ingy@ingy.net>
+# license:   perl
+# copyright: 2011
+
+use 5.010;
+use strict;
+use warnings;
+
+package Pegex::Pegex::Emitter::Perl6Regex;
+
+1;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
